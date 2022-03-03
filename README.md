c2d7be0f56dd7ee295def76814fedee1aca80220
enVives <perejoanvm@gmail.com>
2022-03-03 20:57:41 +0100

    canvi

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # SO-II
 Práctica Sistemas Operativos
 bon dia
+<<<<<<< HEAD
+muriqi padre
+=======
 xxxxxxxxx
+>>>>>>> 674879bbe841a188ae1bc67c4763391747acea48
--- mi_mkfs.c
+++ mi_mkfs.c
@@ -7,14 +7,15 @@
 //  argv[2]=nbloques (puede sernos útil la función atoi() para obtener el
 //  valor numérico a partir del string)
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
     int fd = bmount(argv[1]);
-    unsigned char *buf;
+    unsigned char *buf[BLOCKSIZE];
     memset(buf, 0, BLOCKSIZE);
-    for (int i = 0; i < *argv[2]; i++)
+    for (int i = 0; i < atoi(argv[2]); i++)
     {
         bwrite(i, buf);
     }
     bumount();
+    
 }
