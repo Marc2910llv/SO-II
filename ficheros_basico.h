4eb5aedbe5e0202867474b263f202f2d1ca379a8
Marc Llobera <marclloberavillalonga@gmail.com>
2022-03-10 13:47:11 +0100

    funcions afegides a ficheros_basico.h

--- ficheros_basico.h
+++ ficheros_basico.h
@@ -58,3 +58,10 @@ struct inodo
 int initSB(unsigned int nbloques, unsigned int ninodos);
 int initMB();
 int initAI();
+int escribir_bit(unsigned int nbloque, unsigned int bit);
+char leer_bit(unsigned int nbloque);
+int reservar_bloque();
+int liberar_bloque(unsigned int nbloque);
+int escribir_inodo(unsigned int ninodo, struct inodo inodo);
+int leer_inodo(unsigned int ninodo, struct inodo *inodo);
+int reservar_inodo(unsigned char tipo, unsigned char permisos);
