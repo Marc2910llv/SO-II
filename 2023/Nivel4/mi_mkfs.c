d49102649dbfe947c9c399ee6f269f58f43dc165
Marc Llobera Villalonga <marclloberavillalonga@gmail.com>
2023-03-09 19:13:32 +0100

    Nivel4

---
+++ 2023/Nivel4/mi_mkfs.c
@@ -0,0 +1,61 @@
+/*
+ *   Marc Llobera Villalonga
+ *
+ *   mi_mkfs.c {main()}
+ */
+
+#include "ficheros_basico.h"
+
+int main(int argc, char **argv)
+{
+    if (argc != 3)
+    {
+        perror("Error Sintaxis");
+        return FALLO;
+    }
+
+    unsigned char buf[BLOCKSIZE];
+    memset(buf, 0, BLOCKSIZE);
+
+    if (bmount(argv[1]) == FALLO)
+    {
+        perror("Error main bmount");
+        return FALLO;
+    }
+
+    for (int i = 0; i < atoi(argv[2]); i++)
+    {
+        if (bwrite(i, buf) == FALLO)
+        {
+            perror("Error main bwrite");
+            return FALLO;
+        }
+    }
+
+    if (initSB(atoi(argv[2]), (atoi(argv[2]) / 4)) == FALLO)
+    {
+        perror("Error main initSB");
+        return FALLO;
+    }
+    if (initMB() == FALLO)
+    {
+        perror("Error main initMB");
+        return FALLO;
+    }
+    if (initAI() == FALLO)
+    {
+        perror("Error main initAI");
+        return FALLO;
+    }
+
+    if (reservar_inodo('d', 7) == FALLO)
+    {
+        perror("Error main reservar_inodo");
+        return FALLO;
+    }
+
+    if (bumount() == FALLO)
+    {
+        perror("Error main bumount");
+    }
+}
