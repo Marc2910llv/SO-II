6a76834e7287b4b0e0e5e5268c45b7739fc47c13
Marc Llobera Villalonga <marclloberavillalonga@gmail.com>
2023-03-23 15:38:32 +0100

    Nivel 5

--- 2023/Nivel5/escribir.c
+++ 2023/Nivel5/escribir.c
@@ -8,65 +8,63 @@
 
 int main(int argc, char *argv[])
 {
-    int numero;                                                     // ninodo
-    int cantidad;                                                   // cantidad de bytes escritos
-    int offset[5] = {9000, 209000, 30725000, 409605000, 480000000}; // offsets
     if (argc != 4)
     {
         printf("\x1b[31m", "Sintaxis: escribir <nombre_dispositivo> <'$(cat fichero)'> <diferentes_inodos>\n");
         printf("Offsets: 9000, 209000, 30725000, 409605000, 480000000 \n");
         printf("Si diferentes_inodos=0 se reserva un solo inodo para todos los offsets\n");
+        return FALLO;
     }
-    else
+    int offset[5] = {9000, 209000, 30725000, 409605000, 480000000}; // offsets
+
+    printf("Longitud texto: %ld \n\n", strlen(argv[2]));
+
+    if (bmount(argv[1]) == FALLO)
     {
-        printf("Longitud texto: %ld \n\n", strlen(argv[2]));
-
-        if (bmount(argv[1]) == -1) // abrimos el archivo
-        {
-            perror("ERROR EN ecribir_c AL INTENTAR ABRIR EL FICHERO");
-            return -1;
-        }
-        numero = reservar_inodo('f', 6);
-        if (numero == -1)
-        {
-            perror("ERROR EN escribir_c AL INTENTAR RESERVAR UN INODO");
-            return -1;
-        }
-
-        for (int i = 0; i < 5; i++) // bucle para cada offset;
-        {
-            printf("Numero inodo reservado: %d \n", numero);
-            cantidad = mi_write_f(numero, argv[2], offset[i], strlen(argv[2]));
-            printf("Offset: %d \n", offset[i]);
-            if (cantidad == -1)
-            {
-                perror("ERROR EN escribir_c EN mi_write_f");
-                return -1;
-            }
-
-            printf("Bytes escritos: %d \n", cantidad);
-            struct STAT stat;
-            if (mi_stat_f(numero, &stat) == -1)
-            {
-                perror("ERROR EN escribir_c AL INTENTAR mi_stat_f");
-                return -1;
-            }
-
-            if (atoi(argv[3]) != 0)
-            { // si es diferent de 0, per a cada offset reservarem un inodo diferent
-                numero = reservar_inodo('f', 6);
-            }
-
-            printf("stat.tamEnBytesLog = %d\n", stat.tamEnBytesLog);
-            printf("stat.numBloquesOcupados = %d\n", stat.numBloquesOcupados);
-            printf("\n");
-        }
-
-        if (bumount() == -1)
-        {
-            perror("ERROR EN escribir_c AL INTENTAR CERRAR EL FICHERO");
-            return -1;
-        }
-
-        return 0;
+        perror("Error main bmount");
+        return FALLO;
     }
+
+    int ninodo = reservar_inodo('f', 6);
+
+    if (ninodo == FALLO)
+    {
+        perror("Error main reservar_inodo");
+        return FALLO;
+    }
+
+    for (int i = 0; i < len(offset); i++)
+    {
+        size_t bytesEscritos = mi_write_f(ninodo, argv[2], offset[i], strlen(argv[2]));
+        if (bytesEscritos == FALLO)
+        {
+            perror("Error main mi_write_f");
+            return FALLO;
+        }
+
+        struct STAT stat;
+        if (mi_stat_f(n, &stat) == FALLO)
+        {
+            perror("Error main mi_stat_f");
+            return FALLO;
+        }
+
+        printf("Numero inodo reservado: %d\n", ninodo);
+        printf("Offset: %d\n", offset[i]);
+
+        printf("Bytes escritos: %d \n", bytesEscritos);
+        printf("stat.tamEnBytesLog = %d\n", stat->tamEnBytesLog);
+        printf("stat.numBloquesOcupados = %d\n", stat->numBloquesOcupados);
+        printf("\n");
+
+        if (atoi(argv[3]) != 0)
+        {
+            ninodo = reservar_inodo('f', 6);
+        }
+    }
+
+    if (bumount() == FALLO)
+    {
+        perror("Error main bumount");
+    }
+}
---
+++ 2023/Nivel5/leer.c
@@ -0,0 +1,66 @@
+/*
+ *   Marc Llobera Villalonga
+ *
+ *   Programa de pruebas
+ */
+
+#include "ficheros.h"
+
+#define tambuffer 1500 // tamaño que vamos a leer
+
+int main(int argc, char const *argv[])
+{
+    if (argc < 3)
+    {
+        printf("Sintaxis : leer <nombre_dispositivo> <ninodo>\n ");
+        return FALLO;
+    }
+
+    if (bmount(argv[1]) == FALLO)
+    {
+        perror("Error main bmount");
+        return FALLO;
+    }
+
+    int ninodo = atoi(argv[2]);
+    size_t bytesLeidos = 0;
+    char buffer_texto[tambuffer];
+    int offset = 0; // empezamos en el offset 0
+
+    memset(buffer_texto, 0, tambuffer);
+    size_t leidos = mi_read_f(ninodo, buffer_texto, offset, tambuffer);
+    if (leidos == FALLO)
+    {
+        perror("Error main mi_read_f (fuera del bucle)");
+        return FALLO;
+    }
+    while (leidos > 0)
+    {
+        bytesLeidos = bytesLeidos + leidos;
+        write(1, buffer_texto, leidos);
+        memset(buffer_texto, 0, tambuffer);
+
+        offset = offset + tambuffer;
+        leidos = mi_read_f(ninodo, buffer_texto, offset, tambuffer);
+        if (leidos == FALLO)
+        {
+            perror("Error main mi_read_f (dentro del bucle)");
+            return FALLO;
+        }
+    }
+
+    union _inodo inodo;
+    if (leer_inodo(ninodo, &inodo) == FALLO)
+    {
+        perror("Error main leer_inodo");
+        return FALLO;
+    }
+    
+    fprintf(stderr, "total_leidos: %d\n", bytesLeidos);
+    fprintf(stderr, "tamEnBytesLog: %d\n\n", inodo.tamEnBytesLog);
+
+    if (bumount() == FALLO)
+    {
+        perror("Error main bumount");
+    }
+}
