3b0f6484d3fc6f8540f506715c76bcb4d664f3c9
Marc Llobera Villalonga <marclloberavillalonga@gmail.com>
2023-03-27 20:09:33 +0200

    Nivel 6

---
+++ 2023/Nivel6/leer.c
@@ -0,0 +1,75 @@
+/*
+ *   Marc Llobera Villalonga
+ *
+ *   Programa de pruebas
+ */
+
+#include <stdlib.h>
+#include "ficheros.h"
+
+int main(int argc, char const *argv[])
+{
+    int tambuffer = 1500;
+
+    if (argc != 3)
+    {
+        fprintf(stderr, "Sintaxis : leer <nombre_dispositivo> <ninodo>\n ");
+        return FALLO;
+    }
+
+    if (bmount(argv[1]) == FALLO)
+    {
+        fprintf(stderr, "Error main bmount");
+        return FALLO;
+    }
+
+    struct superbloque SB;
+    if (bread(0, &SB) == FALLO)
+    {
+        fprintf(stderr, "Error main bread");
+        return FALLO;
+    }
+
+    int ninodo = atoi(argv[2]);
+    int bytesLeidos = 0;
+    char buffer_texto[tambuffer];
+    int offset = 0; // empezamos en el offset 0
+
+    memset(buffer_texto, 0, tambuffer);
+    int leidos = mi_read_f(ninodo, buffer_texto, offset, tambuffer);
+    if (leidos == FALLO)
+    {
+        fprintf(stderr, "Error main mi_read_f (fuera del bucle)");
+        return FALLO;
+    }
+
+    while (leidos > 0)
+    {
+        bytesLeidos = bytesLeidos + leidos;
+        write(1, buffer_texto, leidos);
+        memset(buffer_texto, 0, tambuffer);
+        offset = offset + tambuffer;
+        leidos = mi_read_f(ninodo, buffer_texto, offset, tambuffer);
+        if (leidos == FALLO)
+        {
+            fprintf(stderr, "Error main mi_read_f (dentro del bucle)");
+            return FALLO;
+        }
+    }
+
+    union _inodo inodo;
+    if (leer_inodo(ninodo, &inodo) == FALLO)
+    {
+        fprintf(stderr, "Error main leer_inodo");
+        return FALLO;
+    }
+
+    fprintf(stderr, "total_leidos: %d\ntamEnBytesLog: %d\n", bytesLeidos, inodo.tamEnBytesLog);
+
+    if (bumount() == FALLO)
+    {
+        fprintf(stderr, "Error main bumount");
+        return FALLO;
+    }
+    return EXITO;
+}
---
+++ 2023/Nivel6/truncar.c
@@ -0,0 +1,3 @@
+/*
+ *   Marc Llobera Villalonga
+ */
