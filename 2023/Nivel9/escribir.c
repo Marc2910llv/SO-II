b8367c610cff46804a5a50ca854830c4537de11c
Marc Llobera Villalonga <marclloberavillalonga@gmail.com>
2023-04-27 18:45:41 +0200

    Nivel 9

---
+++ 2023/Nivel9/escribir.c
@@ -0,0 +1,82 @@
+/*
+ *   Marc Llobera Villalonga
+ */
+
+#include <stdio.h>
+#include "ficheros.h"
+
+int main(int argc, char *argv[])
+{
+    if (argc != 4)
+    {
+        fprintf(stderr, RED "Sintaxis: escribir <nombre_dispositivo> <'$(cat fichero)'> <diferentes_inodos>\n");
+        fprintf(stderr, RED "Offsets: 9000, 209000, 30725000, 409605000, 480000000 \n");
+        fprintf(stderr, RED "Si diferentes_inodos=0 se reserva un solo inodo para todos los offsets\n" RESET);
+        return FALLO;
+    }
+    int offset[5] = {9000, 209000, 30725000, 409605000, 480000000}; // offsets
+
+    printf("Longitud texto: %ld \n\n", strlen(argv[2]));
+
+    if (bmount(argv[1]) == FALLO)
+    {
+        fprintf(stderr, RED "Error main bmount" RESET);
+        return FALLO;
+    }
+
+    int ninodo = reservar_inodo('f', 6);
+    if (ninodo == FALLO)
+    {
+        fprintf(stderr, RED "Error main reservar_inodo" RESET);
+        return FALLO;
+    }
+    if (ninodo == -2)
+    {
+        fprintf(stderr, RED "Error main reservar_inodo (no quedan bloques libres)" RESET);
+        return FALLO;
+    }
+
+    for (int i = 0; i < (sizeof(offset) / sizeof(int)); i++)
+    {
+        printf("Numero inodo reservado: %d\n", ninodo);
+        printf("Offset: %d\n", offset[i]);
+
+        int bytesEscritos = mi_write_f(ninodo, argv[2], offset[i], strlen(argv[2]));
+        if (bytesEscritos == FALLO)
+        {
+            fprintf(stderr, RED "Error main mi_write_f" RESET);
+            return FALLO;
+        }
+
+        struct STAT stat;
+        if (mi_stat_f(ninodo, &stat) == FALLO)
+        {
+            fprintf(stderr, RED "Error main mi_stat_f" RESET);
+            return FALLO;
+        }
+
+        printf("Bytes escritos: %d \n", bytesEscritos);
+        printf("stat.tamEnBytesLog = %d\n", stat.tamEnBytesLog);
+        printf("stat.numBloquesOcupados = %d\n\n", stat.numBloquesOcupados);
+
+        if (strcmp(argv[3], "0"))
+        {
+            ninodo = reservar_inodo('f', 6);
+            if (ninodo == FALLO)
+            {
+                fprintf(stderr, RED "Error main reservar_inodo" RESET);
+                return FALLO;
+            }
+            if (ninodo == -2)
+            {
+                fprintf(stderr, RED "Error main reservar_inodo (no quedan bloques libres)" RESET);
+                return FALLO;
+            }
+        }
+    }
+
+    if (bumount() == FALLO)
+    {
+        fprintf(stderr, RED "Error main bumount" RESET);
+    }
+}
